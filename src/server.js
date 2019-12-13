41db017c899cdde14f77b8e2c8d320fd05819491
Ricardo Tao <ricardotao@gmail.com>
2019-12-12 22:58:43 -0300

    Heroku v1.3

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 3333;
 
 dotenv.config();
 
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
