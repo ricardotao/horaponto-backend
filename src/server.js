f54acd9c553631f834e2d4659390b6d92abb3636
Ricardo Tao <ricardotao@gmail.com>
2019-12-12 22:36:56 -0300

    Heroku v1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
